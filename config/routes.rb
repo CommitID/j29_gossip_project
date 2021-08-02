8fd3ec9dcaee0499a9012c5a58e6a97a9daf7d58
Marin <marin.chretien@gmail.com>
2021-08-02 19:05:54 +0200

    Un peu de data dans tout ça

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/team', to: 'team#index'
   get '/contact', to: 'contact#index'
   get '/welcome/:name', to: 'welcome#print'
+  get '/', to: 'home#index'
 
 end
 
