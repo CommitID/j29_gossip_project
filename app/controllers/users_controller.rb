19d093aa0f304d1365645efa6aff7359746f16ca
Marin <marin.chretien@gmail.com>
2021-08-04 12:31:30 +0200

    Hot User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to users_path(user.id)
+  end
+
+  def edit
+    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+  end
+
+  def update
+    # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params
+    # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+  end
+
+  def destroy
+    # Méthode qui récupère le potin concerné et le détruit en base
+    # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
+  end
+
+  private
+  
+  def post_params
+    params.require(:user).permit(:first_name, :last_name, :description, :email, :age, :city_id, :slug)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/', to: 'gossips#index'
 
   resources :gossips
-
+  resources :users
 end
 
 
